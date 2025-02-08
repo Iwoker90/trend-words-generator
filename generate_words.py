00d4501501d1943e31f0118f741fd2a3eb936ab2
Iwoker90 <84662061+Iwoker90@users.noreply.github.com>
2025-02-09 00:53:53 +0100

    Update generate_words.py

--- generate_words.py
+++ generate_words.py
@@ -1,21 +1,22 @@
 from pytrends.request import TrendReq
 
-# Inizializza pytrends
+# Inizializzazione
 pytrends = TrendReq(hl='it', tz=360)
 
-# Funzione per ottenere le tendenze di ricerca in Italia
+# Funzione per ottenere le ricerche di tendenza in Italia
 def get_trending_searches():
-    trending_searches = pytrends.trending_searches(pn='italy')  # Ottieni direttamente le tendenze in Italia senza build_payload
-    return trending_searches
+    pytrends.build_payload(kw_list=[], geo='IT', timeframe='now 1-d')
+    trending_searches = pytrends.trending_searches()
+    
+    # Prendi i primi 100 trend
+    return trending_searches.head(100).values.flatten()
 
+# Funzione principale
 if __name__ == '__main__':
     searches = get_trending_searches()
-
-    # Se non sono state trovate tendenze, stampa un messaggio
-    if not searches.empty:
-        searches_list = searches[0].tolist()  # Converte la serie pandas in lista
-        with open('keywords.txt', 'w') as f:
-            f.write('\n'.join(searches_list))
-        print("File 'keywords.txt' aggiornato con le tendenze di ricerca!")
-    else:
-        print("Nessuna tendenza di ricerca trovata.")
+    
+    # Scrivi i risultati nel file
+    with open('keywords.txt', 'w') as f:
+        f.write('\n'.join(searches))
+    
+    print("File 'keywords.txt' aggiornato con 100 ricerche!")
