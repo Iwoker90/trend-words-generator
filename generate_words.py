3cb17ce6e4561847c3e744f01063189f89d8be56
Iwoker90 <84662061+Iwoker90@users.noreply.github.com>
2025-02-09 00:37:32 +0100

    Update generate_words.py

--- generate_words.py
+++ generate_words.py
@@ -1,21 +1,11 @@
 import requests
-from collections import Counter
-import string
-from nltk import ngrams
 import nltk
 
 # Scarica risorse necessarie per ngrams
 nltk.download('punkt')
 
-# Lista di stopwords italiane (parole comuni da ignorare)
-stopwords = set([
-    "di", "a", "in", "e", "il", "la", "per", "con", "su", "che", "un", "una", 
-    "dei", "della", "alle", "sulle", "dal", "al", "ma", "come", "è", "sono", "quello", 
-    "questo", "si", "non", "perché", "l", "da", "sì", "ha", "questi", "ci", "dopo", 
-    "prima", "come", "bene", "anche", "dovrebbe", "quali", "tra", "questo"
-])
-
-def get_trending_keywords():
+# Funzione per ottenere i titoli principali delle notizie italiane
+def get_trending_titles():
     url = 'https://newsapi.org/v2/top-headlines'
     params = {
         'sources': 'google-news-it',  # Filtro per le notizie italiane di Google News
@@ -40,36 +30,21 @@ def get_trending_keywords():
 
     articles = data['articles']
 
-    # Estrai le parole più significative dai titoli e descrizioni
-    keywords = []
+    # Estrai i titoli principali delle notizie
+    titles = []
     for article in articles:
         title = article['title']
-        description = article.get('description', '')  # Usa una stringa vuota se 'description' è None
+        titles.append(title)
 
-        # Filtriamo e puliamo i titoli e le descrizioni dalle stopwords
-        title_words = [word.lower() for word in title.split() if word.lower() not in stopwords and word not in string.punctuation]
-        description_words = [word.lower() for word in description.split() if word.lower() not in stopwords and word not in string.punctuation]
-
-        # Aggiungiamo le parole significative alla lista
-        keywords.extend(title_words + description_words)
-
-    # Estraiamo i bigrammi (combinazioni di 2 parole)
-    bigrams = ngrams(keywords, 2)
-
-    # Conta la frequenza dei bigrammi
-    bigram_counts = Counter(bigrams)
-
-    # Prendi solo i bigrammi più comuni
-    unique_keywords = [' '.join(bigram) for bigram, _ in bigram_counts.most_common(30)]
-    return unique_keywords
+    return titles
 
 if __name__ == '__main__':
-    keywords = get_trending_keywords()
+    titles = get_trending_titles()
 
-    # Se non sono state trovate parole, stampa un messaggio
-    if not keywords:
-        print("Nessuna parola trovata.")
+    # Se non sono stati trovati titoli, stampa un messaggio
+    if not titles:
+        print("Nessun titolo trovato.")
     else:
         with open('keywords.txt', 'w') as f:
-            f.write('\n'.join(keywords))
-        print("File 'keywords.txt' aggiornato!")
+            f.write('\n'.join(titles))
+        print("File 'keywords.txt' aggiornato con i titoli principali!")
